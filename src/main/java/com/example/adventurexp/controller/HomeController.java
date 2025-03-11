76a0cdb9c877ba72cef1c4a018b186bd0134897e
Ali <alku0001@stud.kea.dk>
2025-03-11 18:04:04 +0100

    Backend henter nu alle aktiviteter

--- src/main/java/com/example/adventurexp/controller/HomeController.java
+++ src/main/java/com/example/adventurexp/controller/HomeController.java
@@ -9,14 +9,14 @@ import com.example.adventurexp.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 
 // CrossOrigin definerer hvor requesten må komme fra
 @RestController
-@CrossOrigin(origins = "https://white-cliff-01dae2510.6.azurestaticapps.net/")
+@RequestMapping("/api") // Tilføjet som test fordi https://adventurexp-g5freqhuangfa9ab.northeurope-01.azurewebsites.net gav whitelabel error
+// Prøv herefter linket https://adventurexp-g5freqhuangfa9ab.northeurope-01.azurewebsites.net/api/activities
+@CrossOrigin(origins = "https://white-cliff-01dae2510.6.azurestaticapps.net")
 public class HomeController {
 
     @Autowired
@@ -24,12 +24,6 @@ public class HomeController {
 
     @Autowired
     private ActivityService activityService;
-
-    @Autowired
-    BookingRepo bookingRepo;
-
-    @Autowired
-    ActivityRepo activityRepo;
 
     // Postmapping fortæller at når en frontend server sender request til /activities, så kører denne kode
     @PostMapping("/activities")
@@ -40,32 +34,5 @@ public class HomeController {
     @GetMapping("/activities")
     public List<Activity> getAllActivities() {
         return activityService.findAll(); // Henter alle aktiviteter fra databasen
-    }
-
-    // Bruges ikke til noget
-    @GetMapping("index")
-    public List<Booking> getAllBookings() {
-
-        /*
-        // Her opretter vi en række i Activity-tabellen
-        Activity activity1 = new Activity("Bowling",12, 145, "hat", 123);
-        activityRepo.save(activity1);
-
-
-        Booking booking1 = new Booking();
-        booking1.setTime(LocalTime.now());
-        booking1.setDate(LocalDate.of(2025,3,7));
-        booking1.setDuration(LocalTime.of(1,30));
-        booking1.setParticipants(15);
-        booking1.setSodas(20);
-        booking1.setSweet_Grams(0);
-        booking1.setTshirts(5);
-
-        // Her finder vi en eksisterende række i activity-tabellen og laver en foreign key til den
-        booking1.setActivity(new Activity("Bowling",12, 145, "hat", 123));
-        //booking1.setCustomer("");
-        bookingRepo.save(booking1);
-        */
-        return bookingService.findAll();
     }
 }
