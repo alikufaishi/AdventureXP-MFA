07c8fd5a4f0adce45d8ce3911bda99ea5b5eb48e
marcus <marcus.bstroem@gmail.com>
2025-03-12 13:16:09 +0100

    Manglede getters and setter:))))

--- src/main/java/com/example/adventurexp/model/SalesItem.java
+++ src/main/java/com/example/adventurexp/model/SalesItem.java
@@ -15,4 +15,27 @@ public class SalesItem {
     @Column(nullable = false)
     private int price;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
